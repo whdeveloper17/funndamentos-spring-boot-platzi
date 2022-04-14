c30574a3fde54246f90c0776b3d61457b07babbf
Wilson <whernandez.extreme@gmail.com>
2022-04-14 12:08:47 -0500

    first commit

---
+++ src/main/java/com/fundamentos/springboot/funamentos/FunamentosApplication.java
@@ -0,0 +1,13 @@
+package com.fundamentos.springboot.funamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunamentosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FunamentosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fundamentos/springboot/funamentos/FunamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fundamentos.springboot.funamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunamentosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
